44a71f6b504891e62b6a65a82364fdacc31c63a8
Steve DiDomenico <steve@northwestern.edu>
2016-06-02 04:44:42 +0000

    adding triple relationship table for real now

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create :name => 'Test User', :email => 'test@example.com', :netid => 'testnetid'
+
+Semester.first_or_create [{:year => '2016', :quarter => 'Winter'},
+                 {:year => '2016', :quarter => 'Spring'}]
+
+Room.first_or_create [{:building => 'Foster-Walker', :room_number => '10'},
+             {:building => 'Foster-Walker', :room_number => '11'}]
+
+semester = Semester.first
+room = Room.first
+
+user.users_semesters_rooms.create(semester: semester, room: room)
---
+++ test/models/users_semesters_room_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UsersSemestersRoomTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
